f835b5d7773b4cb430045278577b51615b3cf8ab
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-03 05:09:20 +0000

    add Const.java

--- android/src/net/jiangyouxin/vnotepad/Const.java
+++ android/src/net/jiangyouxin/vnotepad/Const.java
@@ -1,6 +1,6 @@
 package net.jiangyouxin.vnotepad;
 
 public class Const {
-    public static String URL="http://yourdomain.com/vnotepad.php?";
-    public static String TOKEN="yourtoken";
+    public static String URL="http://better9.sinaapp.com/vnotepad.php?";
+    public static String TOKEN="";
 }
