0291808b11fc250b923dacef9cfc026725953d34
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-03 08:19:46 +0000

    use FileUtility in MainActivity

--- android/src/net/jiangyouxin/vnotepad/MainActivity.java
+++ android/src/net/jiangyouxin/vnotepad/MainActivity.java
@@ -65,43 +65,22 @@ public class MainActivity extends Activity {
         }.start();
     }
 
-    private boolean loadFile() {
-        String res = null;
-        FileInputStream fin = null;
-        try {
-            fin = openFileInput(FILENAME);
-            int length = fin.available();
-            byte[] buffer = new byte[length];
-            fin.read(buffer);
-            res = new String(buffer, "utf8");
-        } catch (Exception e) {
-        } finally {
-            if (fin != null) try {
-                fin.close();
-            } catch (IOException e) {
+    private void loadFile() {
+        byte[] buffer = FileUtility.readFile(this, FILENAME);
+        if (buffer.length > 0) {
+            try {
+                String res = new String(buffer, "utf8");
+                edit.setText(res);
+            } catch (UnsupportedEncodingException e) {
             }
         } 
-        if (res != null) {
-            edit.setText(res);
-            return true;
-        } else {
-            return false;
-        }
     }
 
-    private boolean saveFile() {
+    private void saveFile() {
         String res = edit.getText().toString();
-        FileOutputStream fout = null;
         try {
-            fout = openFileOutput(FILENAME, MODE_PRIVATE);
-            fout.write(res.getBytes("utf8"));
-        } catch (Exception e) {
-        } finally {
-            if (fout != null) try {
-                fout.close();
-            } catch (IOException e) {
-            }
+            FileUtility.writeFile(this, FILENAME, res.getBytes("utf8"));
+        } catch (UnsupportedEncodingException e) {
         }
-        return true;
     }
 }
