676bf4284fea45243c7a4d84ece86bafe788b69b
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-03 04:05:20 +0000

    add sae

---
+++ sae/vnotepad.php
@@ -0,0 +1,14 @@
+<?php
+$kv = new SaeKV();
+$ret = $kv->init();
+if ($ret == true) {
+	if ($_GET["action"] == "download") {
+		$content = $kv->get("vnotepad");
+		print $content;
+	} else {
+		$content = $_POST("vnotepad");
+		$kv->set("vnotepad", $content);
+		print "SUCESSS.";
+	}
+}
+?>
