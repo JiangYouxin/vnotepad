0aeabb4ef88305096b896791fc7552537eeba126
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-03 09:30:44 +0000

    server

--- sae/vnotepad.php
+++ sae/vnotepad.php
@@ -5,10 +5,25 @@ if ($ret == true) {
 	if ($_GET["action"] == "download") {
 		$content = $kv->get("vnotepad");
 		print $content;
-	} else {
+	} else if ($_GET["action"] == "upload"){
 		$content = $_POST["vnotepad"];
 		$kv->set("vnotepad", $content);
 		print "SUCESSS.";
-	}
+	} else {
+		$content = $kv->get("vnotepad");
+		$output = htmlentities ($content, ENT_QUOTES, 'UTF-8');
+?><html>
+<head>
+<title>VNOTEPAD</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<form action="vnotepad.php?action=upload" method="post">
+<input type="submit" value="Upload"/>
+<textarea rows="25" cols="80" name="vnotepad"><?=$output?></textarea>
+</form>
+</body>
+</html>
+<?	}
 }
 ?>
